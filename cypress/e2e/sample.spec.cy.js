8e2dd954e01c5b208ea93b1326efa548ef76594e
josdem <joseluis.delacruz@gmail.com>
2022-06-11 16:50:19 -0400

    Completing test demo

--- cypress/e2e/sample.spec.cy.js
+++ cypress/e2e/sample.spec.cy.js
@@ -1,7 +1,14 @@
 describe("My first test", function() {
 
-    it("Does not do much", function() {
-        expect(true).to.be.equal(true)
+    it("Gets, types and asserts", function() {
+        cy.visit("https://example.cypress.io")
+        cy.contains('type').click()
+        cy.url()
+            .should('include', '/commands/actions')
+        cy.get(".action-email")
+            .type("contact@josdem.io")
+            .should('have.value', 'contact@josdem.io')
+
     })
         
 })
